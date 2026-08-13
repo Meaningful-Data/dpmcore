153f9c300271a11bda2c52d97d24a7208ece5d06
Mario Cortés Ruiz <mario.cortes@meaningfuldata.eu>
2026-08-13 11:22:40 +0200

    fix: use == instead of IS for boolean literal comparisons

--- src/dpmcore/dpm_xl/model_queries.py
+++ src/dpmcore/dpm_xl/model_queries.py
@@ -1400,7 +1400,7 @@ class ViewDatapointsQuery:
                 TableVersionCell,
                 and_(
                     TableVersionCell.table_vid == TableVersion.table_vid,
-                    TableVersionCell.is_void.is_(False),
+                    TableVersionCell.is_void == False,  # noqa: E712
                 ),
             )
             .outerjoin(
